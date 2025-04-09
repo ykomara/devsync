c53ebc9b40c40584647be17e1a66ba7b46798309
yayakomara <148052617+yayakomara@users.noreply.github.com>
2025-04-09 15:47:58 +0200

    configuration render

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -151,3 +151,5 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS: True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
