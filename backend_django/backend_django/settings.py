86ab32ed65ad2a35d611e4eb9885549a627b7abc
yayakomara <148052617+yayakomara@users.noreply.github.com>
2025-04-10 15:11:18 +0200

    config JWT

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
-
-
 ]
 
 REST_FRAMEWORK = {
@@ -56,6 +54,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 MIDDLEWARE = [
